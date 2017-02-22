8f53f0f9749f78b3bb5ab795fd1a6cd0b922e0e3
made <a.grandin42@gmail.com>
2017-02-22 11:41:58 +0100

    add Entity and translation structure file

---
+++ Entity/Scale.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Victoire\Widget\SnakeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Victoire\Bundle\MediaBundle\Entity\Media;
+use Victoire\Bundle\CoreBundle\Annotations as VIC;
+
+/**
+ * Class Scale
+ * @package Victoire\Widget\SnakeBundle\Entity
+ *
+ * @ORM\Table("vic_widget_snake_scale")
+ * @ORM\Entity
+ */
+class Scale
+{
+    use \Victoire\Bundle\WidgetBundle\Entity\Traits\LinkTrait;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, nullable=true)
+     * @VIC\ReceiverProperty("textable")
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     * @VIC\ReceiverProperty("textable")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="\Victoire\Bundle\MediaBundle\Entity\Media")
+     * @ORM\JoinColumn(name="image_id", referencedColumnName="id", onDelete="CASCADE")
+     * @VIC\ReceiverProperty("imageable")
+     */
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="WidgetSnake", inversedBy="scales")
+     * @ORM\JoinColumn(name="snake_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $snake;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="link_label", type="string", length=55, nullable=true)
+     */
+    private $linkLabel;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Scale
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Scale
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     * @return Scale
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSnake()
+    {
+        return $this->snake;
+    }
+
+    /**
+     * @param string $snake
+     * @return Scale
+     */
+    public function setSnake($snake)
+    {
+        $this->snake = $snake;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLinkLabel()
+    {
+        return $this->linkLabel;
+    }
+
+    /**
+     * @param string $linkLabel
+     * @return Scale
+     */
+    public function setLinkLabel($linkLabel)
+    {
+        $this->linkLabel = $linkLabel;
+        return $this;
+    }
+}
---
+++ Entity/WidgetSnake.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Victoire\Widget\SnakeBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Victoire\Bundle\WidgetBundle\Entity\Widget;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class WidgetSnake
+ * @package Victoire\Widget\SnakeBundle\Entity
+ *
+ * @ORM\Table("vic_widget_snake")
+ * @ORM\Entity
+ */
+class WidgetSnake extends Widget
+{
+    /**
+     * @var ArrayCollection
+     * @ORM\OneToMany(targetEntity="Scale", mappedBy="snake", cascade={"persist", "remove"}, orphanRemoval=true)
+     */
+    private $scales;
+
+    /**
+     * WidgetSnake constructor.
+     */
+    public function __construct()
+    {
+        $this->scales = new ArrayCollection();
+    }
+
+    /**
+     * @param Scale $scale
+     * @return $this
+     */
+    public function addScales(Scale $scale)
+    {
+        $this->scales->add($scale);
+        return $this;
+    }
+
+    /**
+     * @param Scale $scale
+     * @return $this
+     */
+    public function removeScale(Scale $scale)
+    {
+        $this->scales->remove($scale);
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getScales()
+    {
+        return $this->scales;
+    }
+
+    /**
+     * @param ArrayCollection $scales
+     * @return WidgetSnake
+     */
+    public function setScales($scales)
+    {
+        $this->scales = $scales;
+        return $this;
+    }
+}
