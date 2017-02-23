6144ea3e61c49418db216a432c858dac0052ce2a
made <a.grandin42@gmail.com>
2017-02-23 14:24:57 +0100

    inverse scales order every two row

--- Resolver/WidgetSnakeContentResolver.php
+++ Resolver/WidgetSnakeContentResolver.php
@@ -18,6 +18,26 @@ class WidgetSnakeContentResolver extends BaseWidgetContentResolver
     public function getWidgetStaticContent(Widget $widget)
     {
         $parameters = parent::getWidgetStaticContent($widget);
+
+        $tmpScale = null;
+        $reversedRow = false;
+
+        foreach ($parameters['scales'] as $key => $scale) {
+            if ($key % 2 == 0 && $reversedRow == true) {
+                $tmpScale = $scale;
+            } elseif ($key % 2 == 1 && $reversedRow == true) {
+                $parameters['scales'][$key-1] = $scale;
+                $parameters['scales'][$key] = $tmpScale;
+                $reversedRow = false;
+                $tmpScale = null;
+            } elseif ($key % 2 == 1 && $reversedRow == false && $key != 0) {
+                $parameters['scales'][$key] = $scale;
+                $reversedRow = true;
+            } else {
+                $parameters['scales'][$key] = $scale;
+            }
+        }
+
         return $parameters;
     }
 }
